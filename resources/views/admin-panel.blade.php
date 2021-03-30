87320e84a78402ce7ce7fa1a00010ed8a70f5c6e
unknown <imamovica1397@gmail.com>
2021-03-31 00:20:41 +0200

    Admin panel prototyping #3

--- resources/views/admin-panel.blade.php
+++ resources/views/admin-panel.blade.php
@@ -3,20 +3,28 @@
 @section('content')    
     <div class="w-full min-h-full flex flex-col">
 
-        <div class="bg-gray-300 h-screen fixed top-0 left-0 xl:w-3/12 xl:bg-gray-50 2xl:w-2/12">
+        <div class="bg-gray-300 h-screen fixed top-0 left-0 xl:w-3/12 xl:bg-gray-50 2xl:w-2/12 border-r-2 border-gray-200 ">
 
-            <div class="xl:py-12 xl:px-6 font-semibold">
+            <div class="xl:py-12 xl:px-10 font-semibold">
                 <h3 class="text-2xl">Admin panel</h3>
             </div>
 
             <div class="text-base">
 
                 <x-admin-panel.side-menu.item href="/" text="Narudzbe">
-                    <x-icons.truck></x-icons.truck>
+                    <x-icons.truck size="20"></x-icons.truck>
                 </x-admin-panel.side-menu.item>
 
                 <x-admin-panel.side-menu.item href="/" text="Proizvodi">
-                    <x-icons.tv></x-icons.tv>
+                    <x-icons.tv size="20"></x-icons.tv>
+                </x-admin-panel.side-menu.item>
+
+                <x-admin-panel.side-menu.item href="/" text="Akcije u toku">
+                    <x-icons.precent size="20"></x-icons.precent>
+                </x-admin-panel.side-menu.item>
+
+                <x-admin-panel.side-menu.item href="/" text="Logout">
+                    <x-icons.logout size="20"></x-icons.logout>
                 </x-admin-panel.side-menu.item>
 
             </div>
@@ -24,6 +32,11 @@
         </div>
 
         <div class="bg-gray-100 min-h-screen xl:w-9/12 2xl:w-10/12 self-end">
+
+            <div>
+
+            </div>
+
         </div>
     </div>
 @endsection
--- resources/views/components/admin-panel/side-menu/item.blade.php
+++ resources/views/components/admin-panel/side-menu/item.blade.php
@@ -6,15 +6,15 @@
 
 <div>
     @if ($active == true)
-    <a href="{{ $href }}" class="bg-blue-600 flex text-gray-50 font-semibold py-3 px-6 xl:mb-2">
-        <div class="mr-2">
+    <a href="{{ $href }}" class="bg-blue-600 flex text-gray-50 font-semibold py-3 px-10 xl:mb-2">
+        <div class="mr-2 flex">
             {{ $slot }}
         </div>
         {{ $text }}
     </a>
     @else
-        <a href="{{ $href }}" class="bg-gray-50 flex border-r-4 border-gray-50 font-semibold py-3 px-6 xl:mb-2 hover:text-blue-600 hover:border-blue-600 hover:bg-white hover:shadow-sm">
-            <div class="mr-2">
+        <a href="{{ $href }}" class="bg-gray-50 flex border-r-4 border-gray-50 font-semibold py-3 px-10 xl:mb-2 hover:text-blue-600 hover:border-blue-600 hover:bg-white">
+            <div class="mr-2 flex">
                 {{ $slot }}
             </div>
             {{ $text }}
